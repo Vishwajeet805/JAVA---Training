092bf256a02c7d7dc07b637cad06cb6e7f27bd73
Vishwajeet805 <vishwajeetsingh80558055@gmail.com>
2025-11-09 23:20:40 +0530

    String Question 3

---
+++ JAVA/src/CompareString.java
@@ -0,0 +1,11 @@
+public class CompareString {
+    public static void main(String[] args) {
+
+    System.out.println("8️⃣ Comparing Strings:");
+    String c1 = "Java";
+    String c2 = "java";
+        System.out.println(c1.equals(c2));           // false
+        System.out.println(c1.equalsIgnoreCase(c2)); // true
+        System.out.println(c1.compareTo(c2));
+}
+}
