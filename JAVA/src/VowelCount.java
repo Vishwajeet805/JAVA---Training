81c80c943669afd81946b8fe4cdd04c8659183e1
Vishwajeet805 <vishwajeetsingh80558055@gmail.com>
2025-11-09 23:17:32 +0530

    String Question 2

---
+++ JAVA/src/VowelCount.java
@@ -0,0 +1,11 @@
+public class VowelCount {
+    public static void main(String[] args) {
+        System.out.println("6️⃣ Count vowels in a string:");
+        String input = "Rupesh Kumar";
+        int count = 0;
+        for (char c : input.toLowerCase().toCharArray()) {
+            if ("aeiou".indexOf(c) != -1) count++;
+        }
+        System.out.println("Vowels: " + count);
+    }
+}
