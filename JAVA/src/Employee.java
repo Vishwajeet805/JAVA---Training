06948ce76cb5b9318e74e0ebdeb850a8ea2759c4
Vishwajeet805 <vishwajeetsingh80558055@gmail.com>
2025-11-12 01:42:15 +0530

    Class and Object Question

---
+++ JAVA/src/Employee.java
@@ -0,0 +1,28 @@
+class CorporateMajdoor{
+    String Name;
+    String EmpID;
+    int Salary;
+    void Display(){
+        System.out.println("Name:"+Name);
+        System.out.println("EmpID:"+EmpID);
+        System.out.println("Salary:"+Salary);
+    }
+    void Increment(int x){
+        System.out.println("Initial Salary:"+Salary);
+        int incre=(Salary* x) / 100;
+        Salary+=incre;
+        System.out.println("Salary Increment:"+x);
+        System.out.println("Final Salary:"+Salary);
+    }
+}
+public class Employee {
+    static void main() {
+        CorporateMajdoor Emp1=new CorporateMajdoor();
+        Emp1.Name="Shruti";
+        Emp1.EmpID="SD313";
+        Emp1.Salary=40000;
+        Emp1.Display();
+        Emp1.Increment(20);
+
+    }
+}
