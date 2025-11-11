806fa1df2e76b96bbac55d07cb91a116996ac0a4
Vishwajeet805 <vishwajeetsingh80558055@gmail.com>
2025-11-12 00:04:50 +0530

    Class and Object Question 3

--- JAVA/src/BankAc.java
+++ JAVA/src/BankAc.java
@@ -1,4 +1,37 @@
-package PACKAGE_NAME;
+class BankAccount {
+    int accountNumber;
+    int balance;
+    String accountHolder;
+    void Deposit(int x){
+        balance+=x;
+        System.out.println("Amount Deposited:"+x);
+        System.out.println("Current Balance:"+ balance);
+    }
+    void Withdraw(int x){
+        if(x>balance)
+            System.out.println("Insufficient Balance");
+        else{
+            balance-=x;
+            System.out.println("Amount Withdrawn:"+ x);
+        System.out.println("Current Balance:"+ balance );}
+    }
 
-public class BankAc {
 }
+public class BankAc {
+    static void main(String[] args) {
+        BankAccount Acc1=new BankAccount();
+        Acc1.accountHolder="SHRUTI";
+        Acc1.balance=3000;
+        Acc1.accountNumber=241313;
+        Acc1.Deposit(1000);
+        Acc1.Withdraw(5000);
+        BankAccount Acc2=new BankAccount();
+        Acc2.accountHolder="VISHWAJEET";
+        Acc2.balance=6500;
+        Acc2.accountNumber=241378;
+        Acc2.Deposit(1000);
+        Acc2.Withdraw(5000);
+
+    }
+}
+
