15878be3c16c1b6ba9c05b8451488ad794827f14
Vishwajeet805 <vishwajeetsingh80558055@gmail.com>
2025-11-12 02:00:03 +0530

    Class and Object Question 10

---
+++ JAVA/src/LapTop.java
@@ -0,0 +1,30 @@
+class Laptop {
+    String brand;
+    int ram; // in GB
+    double price;
+
+    // Constructor to initialize values
+    Laptop(String brand, int ram, double price) {
+        this.brand = brand;
+        this.ram = ram;
+        this.price = price;
+    }
+
+    // Method to display laptop details
+    void display() {
+        System.out.println("Brand: " + brand);
+        System.out.println("RAM: " + ram + " GB");
+        System.out.println("Price: ₹" + price);
+        System.out.println("---------------------");
+    }
+}
+
+public class LapTop {
+    public static void main(String[] args) {
+        Laptop l1 = new Laptop("HP", 8, 55000);
+        Laptop l2 = new Laptop("Dell", 16, 72000);
+
+        l1.display();
+        l2.display();
+    }
+}
