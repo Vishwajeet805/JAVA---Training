22a5f500e36675975ebf5463c74dd67e1ed3b091
Vishwajeet805 <vishwajeetsingh80558055@gmail.com>
2025-11-13 13:36:01 +0530

    13 Nov 2006

---
+++ JAVA/src/Employee.java
@@ -0,0 +1,17 @@
+ class Employee {
+    void work(){
+        System.out.println("Employee is working");
+    }
+}
+class Manager extends Employee{
+    void meeting(){
+        System.out.println("Manager is attending a meeting");
+    }
+}
+ class Emloyee{
+     static void main() {
+         Manager m=new Manager();
+         m.work();
+         m.meeting();
+     }
+}
