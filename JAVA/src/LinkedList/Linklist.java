456d5f9dba03cb976b1a9909569e64ce6d323ffb
Vishwajeet805 <vishwajeetsingh80558055@gmail.com>
2025-11-25 23:27:25 +0530

    Added LinkedList

---
+++ JAVA/src/LinkedList/Linklist.java
@@ -0,0 +1,19 @@
+
+package LinkedList;
+
+import java.util.LinkedList;
+
+class link {
+    public static void main(String[] args) {
+
+        LinkedList<Integer> list = new LinkedList<>();
+
+
+        list.add(5);
+        list.add(6);
+        list.addFirst(22);
+        System.out.println(list);
+
+
+    }
+}
