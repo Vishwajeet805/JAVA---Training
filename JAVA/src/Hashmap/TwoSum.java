df80c87ac205a92818804a57476eb99ec2b33ef7
Vishwajeet805 <vishwajeetsingh80558055@gmail.com>
2025-11-26 16:18:00 +0530

    Added Hashmap

--- JAVA/src/Hashmap/TwoSum.java
+++ JAVA/src/Hashmap/TwoSum.java
@@ -1,4 +1,19 @@
 package Hashmap;
 
+
+import java.util.*;
 public class TwoSum {
+    public static void main(String[] args) {
+        int ar[]={2,7,5,4};
+        HashMap<Integer,Integer>map=new HashMap<>();
+        int target=12;
+        for(int i=0;i<ar.length;i++){
+            int diff=target-ar[i];
+            if(map.containsKey(diff)){
+                System.out.println(map.get(diff)+" "+i);
+                break;
+            }
+            map.put(ar[i],i);
+        }
+    }
 }
