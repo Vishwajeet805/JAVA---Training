71182d748b8a2295d88732884c5cced8ff6c50a7
Vishwajeet805 <vishwajeetsingh80558055@gmail.com>
2025-11-29 23:18:32 +0530

    Added Hashset

--- JAVA/src/Hashset/Duplicate.java
+++ JAVA/src/Hashset/Duplicate.java
@@ -6,11 +6,16 @@ class Duplicate {
         int arr[]={1,2,3,3,1};
         HashSet<Integer> set=new HashSet<>();
         HashSet<Integer> dup=new HashSet<>();
+        HashSet<Integer> rep=new HashSet<>();
+
         for (int nums: arr){
             if(!set.add(nums)){
                 dup.add(nums);
             }
         }
+        System.out.println(dup);
+
+
         System.out.println(dup);
 
     }
--- JAVA/src/Hashset/Intersect.java
+++ JAVA/src/Hashset/Intersect.java
@@ -3,14 +3,24 @@ import java.util.HashSet;
 
 class Intersect {
     static void main(String[] args) {
-        int arr[]={1,2,3,4,5};
-        int arr3[]={2,5,3,5};
+        int arr[]={2,3,4,5};
+        int arr3[]={2,5,3,5,1};
 
         HashSet<Integer> set=new HashSet<>();
         HashSet<Integer> union=new HashSet<>();
+        HashSet<Integer> intersect=new HashSet<>();
         for (int nums: arr3){
-            union.add(nums);
+            set.add(nums);
         }
-        System.out.println(union);
+
+        System.out.println(set);
+        for (int nums:arr)
+        {
+         if (set.contains(nums))
+         {
+             intersect.add(nums);
+         }
+        }
+        System.out.println(intersect);
     }
 }
