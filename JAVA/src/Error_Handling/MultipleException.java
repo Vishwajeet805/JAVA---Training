643954f0503de53850ae86d67955a0c0136c2205
Vishwajeet805 <vishwajeetsingh80558055@gmail.com>
2025-12-10 23:45:56 +0530

    Error Handling added

---
+++ JAVA/src/Error_Handling/MultipleException.java
@@ -0,0 +1,15 @@
+package Error_Handling;
+
+public class MultipleException {
+    public static void main(String[] args) {
+        try {
+            int[] arr = new int[3];
+            System.out.println(arr[5]); 
+        } catch (ArithmeticException e) {
+            System.out.println("Arithmetic Error");
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Array Index Error");
+        }
+
+    }
+}
