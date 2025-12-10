5a63b75a6755b5876d59b547c9af702a991d5925
Vishwajeet805 <vishwajeetsingh80558055@gmail.com>
2025-12-10 23:44:43 +0530

    Error Handling added

---
+++ JAVA/src/Error_Handling/Trycatch.java
@@ -0,0 +1,14 @@
+package Error_Handling;
+
+public class Trycatch {
+    public static void main(String[] args) {
+        try {
+            int a = 10 / 0; 
+        } catch (ArithmeticException e) {
+            System.out.println("Cannot divide by zero!");
+        } finally {
+            System.out.println("Always executes");
+        }
+
+    }
+}
