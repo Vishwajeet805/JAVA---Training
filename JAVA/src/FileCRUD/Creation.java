53e3becd90c008a1f70522b0a9929a3fbe89ec3b
Vishwajeet805 <vishwajeetsingh80558055@gmail.com>
2025-12-10 23:33:26 +0530

    FileCRUD added

---
+++ JAVA/src/FileCRUD/Creation.java
@@ -0,0 +1,57 @@
+package FileCRUD;
+import java.io.*;
+
+public class Creation {
+    static void main(String[] args) {
+        try {
+            File file =new File("i.html");
+            if(file.delete()){
+                System.out.println("done "+file.getName());
+            }else{
+                System.out.println("file already created");
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        createData();
+        readFileData();
+        deleteData();
+    }
+
+    static void createData() {
+        try {
+            FileWriter writer=new FileWriter("index.html", true);
+        writer.write("\n I'm F");
+        writer.close();
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+    }
+
+    static void readFileData() {
+        try{
+            FileReader reader=new FileReader("index.html");
+            int ch;
+            while((ch= reader.read())!=-1){
+                System.out.print((char)ch);
+            }
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+    static void deleteData(){
+        try{
+            File file = new File("index.html");
+            if(file.delete()){
+                System.out.println("File deleted"+file.getName());
+            }else {
+
+            }
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
+}
