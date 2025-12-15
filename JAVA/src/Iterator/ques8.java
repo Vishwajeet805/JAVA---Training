73f06fcca17ea6139c9fd59a4e65c9c3246b5644
Vishwajeet805 <vishwajeetsingh80558055@gmail.com>
2025-12-15 22:48:46 +0530

    Added Ques8 Iterator

---
+++ JAVA/src/Iterator/ques8.java
@@ -0,0 +1,19 @@
+package Iterator;
+
+import java.util.*;
+import java.util.ListIterator;
+
+class Ques8 {
+    public static void main(String[] args) {
+        ArrayList<Integer> a = new ArrayList<>();
+        a.add(5);
+        a.add(10);
+        a.add(15);
+        Iterator<Integer> it = a.iterator();
+        while (it.hasNext()) {
+            int x = it.next();
+            if (x % 2 == 0)
+                System.out.println(x);
+        }
+    }
+}
