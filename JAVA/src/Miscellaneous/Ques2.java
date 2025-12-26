504d2432b28bd7e0d39f605dbe8a6146d8c343d4
Vishwajeet805 <vishwajeetsingh80558055@gmail.com>
2025-12-27 03:50:18 +0530

    Added Practice Questions

---
+++ JAVA/src/Miscellaneous/Ques2.java
@@ -0,0 +1,21 @@
+import java.util.Stack;
+
+class NextGreaterElement {
+    static void nextGreater(int[] a) {
+        Stack<Integer> st = new Stack<>();
+
+        for (int i = a.length - 1; i >= 0; i--) {
+            while (!st.isEmpty() && st.peek() <= a[i])
+                st.pop();
+
+            int ans = st.isEmpty() ? -1 : st.peek();
+            st.push(a[i]);
+            System.out.println(a[i] + " -> " + ans);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] a = {4, 5, 2, 10, 8};
+        nextGreater(a);
+    }
+}
