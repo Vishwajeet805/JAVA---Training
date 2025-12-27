ad54bfda9c54b364d7ef423e17f010a4a12cdd53
Vishwajeet805 <vishwajeetsingh80558055@gmail.com>
2025-12-27 23:27:11 +0530

    Added Practice Questions

---
+++ JAVA/src/Miscellaneous/Ques4.java
@@ -0,0 +1,31 @@
+class LongestPalindrome {
+    static String longestPalindrome(String s) {
+        if (s.length() < 2) return s;
+        int start = 0, maxLen = 1;
+
+        for (int i = 0; i < s.length(); i++) {
+            int l = i, r = i;
+            while (l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)) {
+                if (r - l + 1 > maxLen) {
+                    start = l;
+                    maxLen = r - l + 1;
+                }
+                l--; r++;
+            }
+
+            l = i; r = i + 1;
+            while (l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)) {
+                if (r - l + 1 > maxLen) {
+                    start = l;
+                    maxLen = r - l + 1;
+                }
+                l--; r++;
+            }
+        }
+        return s.substring(start, start + maxLen);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(longestPalindrome("babad"));
+    }
+}
