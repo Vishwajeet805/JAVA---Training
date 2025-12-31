da9c9ce0878093ad1131e4a9ebb3137e96056fdc
Vishwajeet805 <vishwajeetsingh80558055@gmail.com>
2025-12-31 22:01:42 +0530

    Added Practice Questions

---
+++ JAVA/src/Miscellaneous/Ques12.java
@@ -0,0 +1,15 @@
+package Miscellaneous;
+class FastPower {
+    static long power(long x, long n) {
+        if (n == 0) return 1;
+        long half = power(x, n / 2);
+        if (n % 2 == 0)
+            return half * half;
+        else
+            return x * half * half;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(power(2, 10));
+    }
+}
